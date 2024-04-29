fe0d7639e11650b8442e8fdf2744a5e4ce92e11c
absgeoviz <155337188+absgeoviz@users.noreply.github.com>
2024-04-29 14:46:42 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 var config = {
   //geojson: "https://absgeoviz.alwaysdata.net/perusahaan.php",
-  geojson: "./data/corridor.geojson",
+  geojson: "./data/ROW_geo.geojson",
   title: "Kayan Hidropower Nusantara",
   layerName: "Perusahaan",
   hoverProperty: "nama_perusahaan",

2d1964904adcea3305660f5ab48f3bac7215e7e5
absgeoviz <155337188+absgeoviz@users.noreply.github.com>
2024-04-29 11:34:42 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var config = {
-//  geojson: "https://absgeoviz.alwaysdata.net/perusahaan.php",
-  geojson: "./data/corridor.geojson",
-  title: "Daftar Perusahaan Corridor",
+  geojson: "https://absgeoviz.alwaysdata.net/perusahaan.php",
+  //geojson: "./data/corridor.geojson",
+  title: "Kayan Hidropower Nusantara",
   layerName: "Perusahaan",
   hoverProperty: "nama_perusahaan",
   sortProperty: "id",
